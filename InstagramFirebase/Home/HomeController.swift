5fabec76f5ceb5ce0fe4d043a1de9d300004c7d7
Chase McElroy <ichasem@gmail.com>
2018-02-21 15:27:22 -0800

    Posted comments to firebase

--- InstagramFirebase/Home/HomeController.swift
+++ InstagramFirebase/Home/HomeController.swift
@@ -82,7 +82,8 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
             dictionaries.forEach({ (key, value) in
                 guard let dictionary = value as? [String:Any] else {return}
                 
-                let post = Post(user: user, dictionary: dictionary)
+                var post = Post(user: user, dictionary: dictionary)
+                post.id = key
                 self.posts.append(post)
             })
             self.posts.sort(by: { (p1, p2) -> Bool in
@@ -135,6 +136,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         print("Message coming from home controller")
         print(post.caption)
         let commentsController = CommentsController(collectionViewLayout: UICollectionViewFlowLayout())
+        commentsController.post = post
         navigationController?.pushViewController(commentsController, animated: true)
     }
 
