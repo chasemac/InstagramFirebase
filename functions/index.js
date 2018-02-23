92a029a59676e9751021f5aa5b73ef0bb1b7dd9f
Chase McElroy <ichasem@gmail.com>
2018-02-23 11:11:26 -0800

    Push notifications for when follow button is tapped

--- functions/index.js
+++ functions/index.js
@@ -7,34 +7,77 @@ admin.initializeApp({
   databaseURL: "https://instagramfirebase-ede01.firebaseio.com/"
 });
 
-exports.sendPushNotifications = functions.https.onRequest((req, res) => {
-  res.send("Attempting to send PUSH notification...")
-  console.log("LOGGER: --- Trying to send PUSH message....");
+// listen for following events and then trigger a push notification
+exports.observeFollowing = functions.database.ref('/following/{uid}/{followingId}').onCreate(event => {
 
-  // As an admin, the app has access to read and write all data, regardless of Security Rules
-  var uid = 'uedniWFX3QgBZrjfWygsnwjxMxY2';
+  var uid = event.params.uid;
+  var followingId = event.params.followingId;
+
+  // let's log out some messages
+  console.log('User: ' + uid + ' is following: ' + followingId)
+
+  //trying to figure out fcmToken to send a push message
   var db = admin.database();
-  var ref = db.ref("/users/" + uid);
-  return ref.once("value", function(snapshot) {
-    var user = snapshot.val();
-    console.log("user name: " + user.username + " fcmToken: " + user.fcmToken);
-    // See documentation on defining a message payload.
-    var payload = {
-      notification: {
-        title: "TITLE OF notification",
-        body: "BODY OF MESSAGE"
-      },
-    };
-    // Send a message to the device corresponding to the provided
-    // registration token.
-    admin.messaging().sendToDevice(user.fcmToken, payload)
-      .then((response) => {
-        // Response is a message ID string.
-        console.log('Successfully sent message:', response);
-        return true
-      }).catch((error) => {
-        console.log('Error sending message:', error);
-        return true
-      });
-  });
+  var ref = db.ref("/users/" + followingId);
+  return ref.once("value", snapshot => {
+
+    var userWeAreFollowing = snapshot.val();
+
+    var refUserName = db.ref("/users/" + uid);
+    return refUserName.once('value', snapshot => {
+
+      var userDoingTheFollowing = snapshot.val();
+
+      var payload = {
+        notification: {
+          title: "You now have a new follower",
+          body: userDoingTheFollowing.username + " is now following you"
+        },
+      };
+
+      admin.messaging().sendToDevice(userWeAreFollowing.fcmToken, payload)
+        .then((response) => {
+          console.log('Successfully sent message:', response);
+          return true
+        }).catch((error) => {
+          console.log('Error sending message:', error);
+          return true
+        });
+
+    })
+
+  })
 })
+
+
+// exports.sendPushNotifications = functions.https.onRequest((req, res) => {
+//   res.send("Attempting to send PUSH notification...")
+//   console.log("LOGGER: --- Trying to send PUSH message....");
+//
+//   // As an admin, the app has access to read and write all data, regardless of Security Rules
+//   var uid = 'uedniWFX3QgBZrjfWygsnwjxMxY2';
+//   var db = admin.database();
+//   var ref = db.ref("/users/" + uid);
+//   return ref.once("value", function(snapshot) {
+//     var user = snapshot.val();
+//     console.log("user name: " + user.username + " fcmToken: " + user.fcmToken);
+//     // See documentation on defining a message payload.
+//     var payload = {
+//       notification: {
+//         title: "TITLE OF notification",
+//         body: "BODY OF MESSAGE"
+//       },
+//     };
+//     // Send a message to the device corresponding to the provided
+//     // registration token.
+//     admin.messaging().sendToDevice(user.fcmToken, payload)
+//       .then((response) => {
+//         // Response is a message ID string.
+//         console.log('Successfully sent message:', response);
+//         return true
+//       }).catch((error) => {
+//         console.log('Error sending message:', error);
+//         return true
+//       });
+//   });
+// })
