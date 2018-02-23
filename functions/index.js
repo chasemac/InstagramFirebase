0caec58995d5aa65595fb85e82fb40b7510aa268
Chase McElroy <ichasem@gmail.com>
2018-02-23 09:51:29 -0800

    Function grabs users FCM token from database and send Push Notification

--- functions/index.js
+++ functions/index.js
@@ -18,64 +18,23 @@ exports.sendPushNotifications = functions.https.onRequest((req, res) => {
   return ref.once("value", function(snapshot) {
     var user = snapshot.val();
     console.log("user name: " + user.username + " fcmToken: " + user.fcmToken);
-
-  })
-
-//   //admin.message().sendToDevice(token, payload)
-//   // This registration token comes from the client FCM SDKs.
-//   var fcmToken = 'ddQrANcEXiU:APA91bEacgimEZyf_-fOW3YaBd8QKzvaB_2iQqnDy7mBLLW3mWBeDntMx7C5JTLS2pYykmA1sRQWhD0Eq39ZPpwHTTr7I038qJDdKnrtFyOieweZP0OzEUbao5x-mR-qRvgeeacAc3iq';
-//
-//   // See documentation on defining a message payload.
-//   var payload = {
-//     notification: {
-//       title: "TITLE OF notification",
-//       body: "BODY OF MESSAGE"
-//     },
-//     data: {
-//       score: '850',
-//       time: '2:45'
-//     },
-//   };
-//
-//   // Set the message as high priority and have it expire after 24 hours.
-// var options = {
-//   priority: "high",
-//   timeToLive: 60 * 60 * 24
-// };
-//
-//   // Send a message to the device corresponding to the provided
-//   // registration token.
-//   admin.messaging().sendToDevice(fcmToken, payload, options)
-//   .then((response) => {
-//       // Response is a message ID string.
-//       console.log('Successfully sent message:', response);
-//       return true
-//     }).catch((error) => {
-//       console.log('Error sending message:', error);
-//       return true
-//     });
-
+    // See documentation on defining a message payload.
+    var payload = {
+      notification: {
+        title: "TITLE OF notification",
+        body: "BODY OF MESSAGE"
+      },
+    };
+    // Send a message to the device corresponding to the provided
+    // registration token.
+    admin.messaging().sendToDevice(user.fcmToken, payload)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+        return true
+      }).catch((error) => {
+        console.log('Error sending message:', error);
+        return true
+      });
+  });
 })
-
-
-
-
-
-
-// exports.sendPushNotifications = functions.https.onRequest((req, res) => {
-//   res.send("Attempting to send push notification....")
-//   console.log("LOGGER ___ Trying to send push message...")
-//
-//   var uid =
-//   var fcmToken= "ddQrANcEXiU:APA91bEacgimEZyf_-fOW3YaBd8QKzvaB_2iQqnDy7mBLLW3mWBeDntMx7C5JTLS2pYykmA1sRQWhD0Eq39ZPpwHTTr7I038qJDdKnrtFyOieweZP0OzEUbao5x-mR-qRvgeeacAc3iq";
-//   var payload = {
-//     notification: {
-//       title: "push",
-//       body: "Push it"
-//     },
-//     data: {
-//       score: "850",
-//       time: "2:45"
-//     }
-//   };
-// })
